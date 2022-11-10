e0913ef40ae60bb8296fdb9dbd2228f8bbe21b35
Andrea Genovese <andregenovese2000@gmail.com>
2022-11-10 18:59:59 +0100

    Add: vue router (error)

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,7 +1,9 @@
 window.Vue = require('vue');
 import Vue from 'vue';
 import App from './views/App';
+import router from './router.js';
 const app = new Vue({
     el: '#root',
     render: h => h(App),
+    router
 });
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -2,12 +2,16 @@
     <div>
         <h1> Work in progress </h1>
         <h2> Sito in costruzione </h2>
+        <MyHeader/>
     </div>
 </template>
 
 <script>
+    import MyHeader from '../components/MyHeader.vue';
     export default {
         name: 'App',
-        components: { }
+        components: { 
+            MyHeader
+        }
     }
 </script>
