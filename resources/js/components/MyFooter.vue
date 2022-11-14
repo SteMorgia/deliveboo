ffb654b0497464226ad4adc4812f599daed442e9
saidmoussadek <saiddus24@gmail.com>
2022-11-14 09:13:05 +0100

    pull filtraggio back

--- resources/js/components/MyFooter.vue
+++ resources/js/components/MyFooter.vue
@@ -1,7 +1,122 @@
 <template>
-  <footer>
-    sono il footer
-  </footer>
+  <!-- Footer -->
+<footer class="text-center text-lg-start  text-muted" style="background-color: rgb(46, 51, 51)">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section>
+    <div class="container text-center text-md-start mt-5 ">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 links">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>deliveboo
+          </h6>
+          <p>
+           Your favourite restaurants and takeaways, delivered to your door
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 links" >
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Discover Deliveroo
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">restaurants</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Dischs</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Takeaway</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">FastFood</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 links">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            legale
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Terms and conditions</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Privacy</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Cookies</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Speak Up</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto  mb-4 links">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-2">Help</h6>
+          <p><a href="#!" class="text-reset">Contact</a></p>
+          <p><a href="#!" class="text-reset">FAQs</a></p>
+          <p><a href="#!" class="text-reset">Cuisines</a></p>
+          <p><a href="#!" class="text-reset">More</a></p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4"  style="background-color: rgb(46, 51, 51) links">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="#">Team Bali</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 </template>
 
 <script>
@@ -11,6 +126,13 @@ export default {
 </script>
 
 <style lang="scss">
+   .links{
+   // border: 2px solid white;
+    background-color: rgb(67, 72, 72);
+    padding-top: 2rem;
+    color:white;
+   
+   }
  
 
 </style>
