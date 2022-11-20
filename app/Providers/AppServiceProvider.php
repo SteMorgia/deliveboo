57f516ffc60479a512e39a5edcbfddf0351f6a25
Luca Somaschini <somas99@gmail.com>
2022-11-21 00:37:42 +0100

    Add: braintree + gateway's singleton with braintree api keys

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Braintree\Gateway; // importo il gateway;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Gateway::class, function($app){
+            return new Gateway(
+                [
+                    'environment' => 'sandbox',
+                    'merchantId' => 'ksqt4zkwdxxnp6qz',
+                    'publicKey' => 'x644ydrhbbt2xdwm',
+                    'privateKey' => '503a9b2e8c7db844d0ff77a995f48fcb'
+                ]
+            );
+        });
     }
 }
