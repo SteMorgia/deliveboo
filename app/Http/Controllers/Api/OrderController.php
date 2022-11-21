fb5263040ff053dc8abcd31254e25ee4e7a713c7
Luca Somaschini <somas99@gmail.com>
2022-11-21 01:10:22 +0100

    Add: generate token through api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,13 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Braintree\Gateway; // importo il gateway;
+
 class OrderController extends Controller
 {
-    public function generateToken() {
-        return 'generate';
+    public function generateToken(Request $request, Gateway $gateway) {
+        
+        $token = $gateway->clientToken()->generate(); // genero primo token per autorizzare la creazione del form di pagamento nel frontend;
+
+        $data = [
+            'succes' => true,
+            'token' => $token
+        ];
+
+        return response()->json($data);
     }
 
-    public function makePayment() {
+    public function makePayment(Request $request, Gateway $gateway) {
         return 'payment';
     }
 }
