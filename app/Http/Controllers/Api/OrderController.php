a9ff5d16d9648ab57436dbe4476e35685cbf3279
Luca Somaschini <somas99@gmail.com>
2022-11-21 01:50:12 +0100

    Add: sale transaction + nonce + response

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -22,6 +22,39 @@ class OrderController extends Controller
     }
 
     public function makePayment(Request $request, Gateway $gateway) {
-        return 'payment';
+
+        // la $request, in ingresso dal frontend tramite metodo POST, conterrà il token NONCE + il carrello;
+
+        // creo una nuova transazione di vendita (le chiavi sono obbligatorie - vedi la documentazione di Braintree);
+        $result = $gateway->transaction()->sale([
+
+            'amount' => '10.00', // sarà il totale del carrello, inviato dal frontend e contenuto nella $request;
+            'paymentMethodNonce' => $request->token, // NON è il token della generateToken(), ma un altro token generato dal plugin del frontend (il form di pagamento) che è contenuto nella $request;
+            'options' => [
+                'submitForSettlement' => true
+            ]
+
+        ]);
+
+        // gestisco il successo o meno della transazione (può fallire perché il token NONCE, inviato a Braintree, non è valido);
+        if ( $result->success ) {
+
+            $data = [
+                'succes' => true,
+                'message' => 'Transazione eseguita con successo!'
+            ];
+
+            return response()->json($data);
+
+        } else {
+
+            $data = [
+                'succes' => false,
+                'message' => 'Transazione fallita!'
+            ];
+
+            return response()->json($data);
+
+        }
     }
 }
