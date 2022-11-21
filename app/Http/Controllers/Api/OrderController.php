cd656acfd2281699a79e9934963aba572bd5de31
Luca Somaschini <somas99@gmail.com>
2022-11-21 23:39:48 +0100

    Add: request

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use Braintree\Gateway; // importo il gateway;
+use App\Http\Requests\Orders\OrderRequest; // importo la classe della request;
 
 class OrderController extends Controller
 {
     public function generateToken(Request $request, Gateway $gateway) {
         
         $token = $gateway->clientToken()->generate(); // genero primo token per autorizzare la creazione del form di pagamento nel frontend;
-
         $data = [
             'succes' => true,
             'token' => $token
@@ -21,14 +21,14 @@ class OrderController extends Controller
         return response()->json($data);
     }
 
-    public function makePayment(Request $request, Gateway $gateway) {
+    public function makePayment(OrderRequest $request, Gateway $gateway) {
 
         // la $request, in ingresso dal frontend tramite metodo POST, conterrà il token NONCE + il carrello;
 
         // creo una nuova transazione di vendita (le chiavi sono obbligatorie - vedi la documentazione di Braintree);
         $result = $gateway->transaction()->sale([
 
-            'amount' => '10.00', // sarà il totale del carrello, inviato dal frontend e contenuto nella $request;
+            'amount' => $request->amount, // sarà il totale del carrello, inviato dal frontend e contenuto nella $request;
             'paymentMethodNonce' => $request->token, // NON è il token della generateToken(), ma un altro token generato dal plugin del frontend (il form di pagamento) che è contenuto nella $request;
             'options' => [
                 'submitForSettlement' => true
